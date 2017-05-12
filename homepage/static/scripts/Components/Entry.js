a5e0aee824a2072328818d98d925106b5ea01316
Andrew Meyer <andrew@eissturm.com>
2017-05-11 20:59:00 -0500

    updates

---
+++ homepage/static/scripts/Components/Entry.js
@@ -0,0 +1,24 @@
+class BlogEntry extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  static get observedAttributes() {return ['data-entry-id']; }
+
+  attributeChangedCallback(attr, oldValue, newValue) {
+    if (attr == 'data-entry-id') {
+      var xhr = new XMLHttpRequest;
+      var entryContent;
+      xhr.onreadystatechange = function() {
+        entryContent = JSON.parse(xhr.responseText);
+        console.log(entryContent);
+        var r = document.querySelector("#root")
+        var blogEntry = document.querySelector("#blog-1")
+        blogEntry.textContent = entryContent['body_raw'];
+      }
+      xhr.open("GET", `http://localhost:7470/api/entries/${newValue}`);
+      xhr.send();
+    }
+  }
+}
+customElements.define('blog-entry', BlogEntry)
